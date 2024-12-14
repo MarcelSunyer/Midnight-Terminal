625ff5888036e62589448a0a13c9ccef6e334cf3
Eric <ericluque2003@gmail.com>
2024-12-14 13:10:53 +0100

    Train Station Scene
    No se tepean bien, rarete

--- Los Skibidies en el tren/Assets/Scripts/Interactuables/StartGame_Button.cs
+++ Los Skibidies en el tren/Assets/Scripts/Interactuables/StartGame_Button.cs
@@ -1,11 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame_Button : MonoBehaviour, IInteractable
 {
+    // Referencias a los prefabs que queremos teletransportar
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    // Nombre de la escena de destino
+    public string targetScene;
+
+    // Posición de teletransporte
+    public Vector3 teleportPosition = new Vector3(-3, 3, 0);
+
     public void Interact()
     {
         Debug.Log("Game will start soon!");
+
+        if (string.IsNullOrEmpty(targetScene))
+        {
+            Debug.LogError("No se ha especificado la escena de destino.");
+            return;
+        }
+
+        // Encuentra todas las instancias activas en la escena de los prefabs especificados
+        TeleportPrefabs(prefab1);
+        TeleportPrefabs(prefab2);
+
+        // Carga la nueva escena
+        SceneManager.LoadScene(targetScene);
+    }
+
+    private void TeleportPrefabs(GameObject prefab)
+    {
+        if (prefab == null)
+        {
+            Debug.LogError("Uno de los prefabs no está asignado en el inspector.");
+            return;
+        }
+
+        // Encuentra todas las instancias de este prefab
+        GameObject[] instances = GameObject.FindGameObjectsWithTag(prefab.tag);
+
+        foreach (GameObject instance in instances)
+        {
+            Debug.Log($"Teleporting {instance.name} to the new scene.");
+            instance.transform.position = teleportPosition; // Cambia la posición de la instancia
+            DontDestroyOnLoad(instance); // Evita que se destruya al cargar la nueva escena
+        }
     }
 }
+
