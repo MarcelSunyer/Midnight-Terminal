68341891a192395571fa50f2d12df2208066a1a3
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-01-13 17:10:46 +0100

    Camara delete

---
+++ Los Skibidies en el tren/Assets/Scripts/ProgressBarToZero.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarToZero : MonoBehaviour
+{
+    private Progress_bar progress_Bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progress_Bar.act = 0;
+    }
+
+
+}
